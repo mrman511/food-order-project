dda4e099e0366b56e9844c1b6353d09106a5703b
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-02-12 13:19:06 -0700

    Created tables for database, ejs templates and routes connecting the different pages

---
+++ db/schema/03_orders.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  order_time NOW()::DATE,
+  time_ready DATE IS NULL
+);

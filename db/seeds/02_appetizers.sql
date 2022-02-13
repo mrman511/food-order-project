7427fa7a2e30f85c3bf4a1406804fe2ae5d1cc2a
bshin132 <bshin132@gmail.com>
2022-02-13 19:34:19 +0000

    fix seeds conflicts

--- db/seeds/02_appetizers.sql
+++ db/seeds/02_appetizers.sql
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-=======
-INSERT INTO food (id, image, title, category, description, price) VALUES (3, image, 'Deep fried spiral-cut whole potato on a skewer', 6.99)
-INSERT INTO food (id, image, title, category, description, price) VALUES (4, image, 'Potato Salad', 'Appetizers', 'Boiled potatoes with a variety of other salad ingredients', 5.99)
-
-
-INSERT INTO food (image, title, category, description, price)
-VALUES ('https://celebrationgeneration.com/wp-content/uploads/2021/05/Cactus-Cut-Potatoes-4.jpg', 'Cactus Cut Fries', 'appetizer', 'Crispy thick cut potato chips. Served with your choice of dipping sauce', 6.99),
-('https://www.corriecooks.com/wp-content/uploads/2018/08/Instant-Pot-Yam-Fries.jpg', 'Yam Fries', 'appetizer', 'Yam friess served with your choice of dipping sauce', 5.99);
-
-INSERT INTO food (id, image, title, category, description, price) VALUES (3, image, 'Deep fried spiral-cut whole potato on a skewer', 6.99);
->>>>>>> 9c65a939dc944aefdef3e6913a483887ebac7bf6
-
 INSERT INTO food (image, title, category, description, price) VALUES ('https://www.cleaneatingmag.com/wp-content/uploads/2013/11/chilli-cheese-fries.jpg?crop=4:3&width=1600','Chilli cheese fries', 'appetizers', 'Our famous chilli, hand cut, cheese fries. Topped with sour cream, green onion, and real bacon bits', 14);
 
 INSERT INTO food (image, title, category, description, price) VALUES ('https://keviniscooking.com/wp-content/uploads/2020/05/Fried-Pickle-Spears-square.jpg','Deep Fried Pickles', 'appertizers', 'Delicious deep fried crispy pickles.', 12);
@@ -19,4 +6,3 @@ INSERT INTO food (image, title, category, description, price) VALUES ('https://k
 INSERT INTO food (image, title, category, description, price) VALUES ( 'https://img.buzzfeed.com/video-api-prod/assets/a1f17189a78c4004acb92bdf092dc711/BFV12958_TornadoPotato-ThumbTextless1080.jpg', 'Tornado Potato', 'appetizers', 'Deep fried spiral-cut whole potato on a skewer', 6.99);
 
 INSERT INTO food (image, title, category, description, price) VALUES ( 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/potato-salad-with-vinaigrette-1613060061.jpg?crop=0.813xw:0.791xh;0.107xw,0.0778xh&resize=640:*', 'Potato Salad', 'appetizers', 'Boiled potatoes with a variety of other salad ingredients', 5.99);
-

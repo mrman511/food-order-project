1b267a11cc172b3e2a4bd9a7a09e983a7230b6ea
Paul Bodner <bodner80@gmail.com>
2022-02-12 19:36:08 -0800

    added main course seeds

--- db/seeds/01_main.sql
+++ db/seeds/01_main.sql
@@ -1,2 +1,6 @@
-INSERT INTO food (id, image, title, category, description, price) VALUES (1, image, 'Potato Pizza', 'Main', 'Potato pizza made with seasonl potatoes and a variety of other pizza ingredients', 16.99)
-INSERT INTO food (id, image, title, category, description, price) VALUES (2, image, 'Cheesy Scalloped Potato', 'Main', 'Sliced potatoes layered in a casserole dish and baked with heavy cream', 19.99)
+INSERT INTO food (image, title, category, description, price) VALUES (image, 'Potato Pizza', 'main', 'Potato pizza made with seasonl potatoes and a variety of other pizza ingredients', 16.99)
+INSERT INTO food (image, title, category, description, price) VALUES (image, 'Cheesy Scalloped Potato', 'main', 'Sliced potatoes layered in a casserole dish and baked with heavy cream', 19.99)
+
+INSERT INTO food (image, title, category, description, price)
+VALUES ("https://www.simplyhappyfoodie.com/wp-content/uploads/2018/11/instant-pot-baked-potato-3.jpg", "Twice Baked Potato", 'main', 'Grandmas home twice baked potato. with your choice of toppings', 11.99),
+('https://img.hellofresh.com/f_auto,fl_lossy,h_640,q_auto,w_1200/hellofresh_s3/image/2019-w17-r11-ribeye-and-roasted-garlic-pan-sauce-2318925d.jpg', "Steak and Mashed Potatos", 'main', '8oz grass fed Filet, matched with our famously creamy mashed potatos', 24.95);

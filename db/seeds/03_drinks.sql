95816016e8bfe9a7f4236b991dee396cf39fb637
Paul Bodner <bodner80@gmail.com>
2022-02-13 10:50:07 -0800

    edited to inserts in appetizers seeds

--- db/seeds/03_drinks.sql
+++ db/seeds/03_drinks.sql
@@ -1,6 +1,46 @@
+<<<<<<< Updated upstream
 INSERT INTO food (id, image, title, category, description, price) VALUES (5, 'image', 'Potato Smoothie', 'drink', 'Smoothie made with potatoes', 3.50)
 INSERT INTO food (id, image, title, category, description, price) VALUES (6, 'image', 'Coconut Smoothie', 'drink', 'Smoothie made with coconuts', 1.50)
+=======
+INSERT INTO food (id, image, title, category, description, price) VALUES (5, image, 'Potato Smoothie', 'Drinks', 'Smoothie made with potatoes', 3.50)
+INSERT INTO food (id, image, title, category, description, price) VALUES (6, image, 'Coconut Smoothie', 'Drinks', 'Smoothie made with coconuts', 1.50)
+
+>>>>>>> Stashed changes
 
 INSERT INTO food (image, title, category, description, price)
 VALUES ('https://shortgirltallorder.com/wp-content/uploads/2020/10/sweet-potato-smoothie-square-500x500.jpg', 'Sweet Potato Smoothie', 'drink', 'Smoothie made with Organic yams grown in Jamaica', 4.5),
 ('https://www.thespruceeats.com/thmb/hKOU6jw-hBQAtvMLr9S6qVitA6E=/1000x562/smart/filters:no_upscale()/gin-tonic-5a8f334b8e1b6e0036a9631d.jpg', 'drink', 'Gin and Tonic', 'drink', 'We may or may not have put potato in this somehow', 8.99);
+<<<<<<< Updated upstream
+=======
+
+INSERT INTO food (id, image, title, category, description, price) VALUES (5, image, 'Potato Smoothie', 'Drinks', 'Smoothie made with potatoes', 3.50);
+
+INSERT INTO food (image, title, category, description, price) VALUES ( 'https://shortgirltallorder.com/wp-content/uploads/2020/10/sweet-potato-smoothie-square-500x375.jpg', 'Potato Smoothie', 'drinks', 'Smoothie made with potatoes', 3.50);
+
+INSERT INTO food (image, title, category, description, price) VALUES ( 'https://cookeatpaleo.com/wp-content/uploads/2013/08/peach-coconut-smoothie-680x950-680x950.jpg', 'Coconut Smoothie', 'drinks', 'Smoothie made with coconuts', 1.50);
+
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://media.istockphoto.com/photos/classic-cocacola-can-picture-id465133878?k=20&m=465133878&s=612x612&w=0&h=vgcvEz4ts5WZQOhKMaKon8mIQVbr20-JhP7DKgo_wD0=','Coca Cola', 'drinks', 'Can of Coca Cola', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://whistlerdinein.com/wp-content/uploads/2020/05/Sprite-e1537139444640.jpg','Sprite', 'drinks', 'Can of Sprite', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/442/080/6000203442080.jpg', 'Mountain Dew', 'drinks', 'Can of Mountian Dew', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Large/192/681/6000200192681.jpg', 'Root Beer', 'drinks', 'Can of Root Beer', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Large/192/680/6000200192680.jpg', 'Gingerale', 'drinks', 'Can of Gingerale', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Large/844/467/6000201844467.jpg', '7UP', 'drinks', 'Can of 7UP', 3);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdFIV96Gt6tSElx6UHkbeJTXE4KBTeLayLOWaLaMHvh6P_RJDEwqFUvZ016QxQN-hR6mE&usqp=CAU', 'Orange Juice', 'drinks', 'Fresh squezzed OJ', 5);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/958/688/6000197958688.jpg','Chocolate Milk', 'drinks', 'Milk 2 Go Chocolate Milk Bottle', 4);
+
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/958/688/6000197958688.jpg','Chocolate Milk', 'drinks', 'Milk 2 Go Chocolate Milk Bottle', 4);
+
+<<<<<<< HEAD
+=======
+INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/958/688/6000197958688.jpg','Chocolate Milk', 'drink', 'Milk 2 Go Chocolate Milk Bottle', 4);
+>>>>>>> 9c65a939dc944aefdef3e6913a483887ebac7bf6
+>>>>>>> Stashed changes

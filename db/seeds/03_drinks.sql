f068c2c0fb45b468702cc2222bcc5f50c5fd4546
Paul Bodner <bodner80@gmail.com>
2022-02-13 12:20:32 -0800

    updated seeds for appetizers and drinks

--- db/seeds/03_drinks.sql
+++ db/seeds/03_drinks.sql
@@ -1,29 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-INSERT INTO food (id, image, title, category, description, price) VALUES (5, 'image', 'Potato Smoothie', 'drink', 'Smoothie made with potatoes', 3.50)
-INSERT INTO food (id, image, title, category, description, price) VALUES (6, 'image', 'Coconut Smoothie', 'drink', 'Smoothie made with coconuts', 1.50)
-=======
-INSERT INTO food (id, image, title, category, description, price) VALUES (5, image, 'Potato Smoothie', 'Drinks', 'Smoothie made with potatoes', 3.50)
-INSERT INTO food (id, image, title, category, description, price) VALUES (6, image, 'Coconut Smoothie', 'Drinks', 'Smoothie made with coconuts', 1.50)
-
->>>>>>> Stashed changes
-
-INSERT INTO food (image, title, category, description, price)
-VALUES ('https://shortgirltallorder.com/wp-content/uploads/2020/10/sweet-potato-smoothie-square-500x500.jpg', 'Sweet Potato Smoothie', 'drink', 'Smoothie made with Organic yams grown in Jamaica', 4.5),
-('https://www.thespruceeats.com/thmb/hKOU6jw-hBQAtvMLr9S6qVitA6E=/1000x562/smart/filters:no_upscale()/gin-tonic-5a8f334b8e1b6e0036a9631d.jpg', 'drink', 'Gin and Tonic', 'drink', 'We may or may not have put potato in this somehow', 8.99);
-<<<<<<< Updated upstream
-=======
-
-INSERT INTO food (id, image, title, category, description, price) VALUES (5, image, 'Potato Smoothie', 'Drinks', 'Smoothie made with potatoes', 3.50);
-
 INSERT INTO food (image, title, category, description, price) VALUES ( 'https://shortgirltallorder.com/wp-content/uploads/2020/10/sweet-potato-smoothie-square-500x375.jpg', 'Potato Smoothie', 'drinks', 'Smoothie made with potatoes', 3.50);
 
-=======
-INSERT INTO food (image, title, category, description, price) VALUES ( 'https://shortgirltallorder.com/wp-content/uploads/2020/10/sweet-potato-smoothie-square-500x375.jpg', 'Potato Smoothie', 'drinks', 'Smoothie made with potatoes', 3.50);
-
->>>>>>> d64aa30255afa92ce9bfde98de6b45c9ab01c11e
 INSERT INTO food (image, title, category, description, price) VALUES ( 'https://cookeatpaleo.com/wp-content/uploads/2013/08/peach-coconut-smoothie-680x950-680x950.jpg', 'Coconut Smoothie', 'drinks', 'Smoothie made with coconuts', 1.50);
-
 
 INSERT INTO food (image, title, category, description, price) VALUES ('https://media.istockphoto.com/photos/classic-cocacola-can-picture-id465133878?k=20&m=465133878&s=612x612&w=0&h=vgcvEz4ts5WZQOhKMaKon8mIQVbr20-JhP7DKgo_wD0=','Coca Cola', 'drinks', 'Can of Coca Cola', 3);
 
@@ -43,12 +20,3 @@ INSERT INTO food (image, title, category, description, price) VALUES ('https://i
 
 
 INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/958/688/6000197958688.jpg','Chocolate Milk', 'drinks', 'Milk 2 Go Chocolate Milk Bottle', 4);
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-INSERT INTO food (image, title, category, description, price) VALUES ('https://i5.walmartimages.ca/images/Enlarge/958/688/6000197958688.jpg','Chocolate Milk', 'drink', 'Milk 2 Go Chocolate Milk Bottle', 4);
->>>>>>> 9c65a939dc944aefdef3e6913a483887ebac7bf6
->>>>>>> Stashed changes
-=======
->>>>>>> d64aa30255afa92ce9bfde98de6b45c9ab01c11e

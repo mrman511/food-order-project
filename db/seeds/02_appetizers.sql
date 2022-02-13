ef8792b1651bc63879e8424cf4992eee87a789fb
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-02-12 21:40:44 -0700

    Populated tables in database, wrote seed files and inserted data into tables

--- db/seeds/02_appetizers.sql
+++ db/seeds/02_appetizers.sql
@@ -1,2 +1,9 @@
-INSERT INTO food (id, image, title, category, description, price) VALUES (3, image, 'Deep fried spiral-cut whole potato on a skewer', 6.99)
-INSERT INTO food (id, image, title, category, description, price) VALUES (4, image, 'Potato Salad', 'Appetizers', 'Boiled potatoes with a variety of other salad ingredients', 5.99)
+INSERT INTO food (id, image, title, category, description, price) VALUES (3, image, 'Deep fried spiral-cut whole potato on a skewer', 6.99);
+
+INSERT INTO food (id, image, title, category, description, price) VALUES (4, image, 'Potato Salad', 'Appetizers', 'Boiled potatoes with a variety of other salad ingredients', 5.99);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://www.cleaneatingmag.com/wp-content/uploads/2013/11/chilli-cheese-fries.jpg?crop=4:3&width=1600','Chilli cheese fries', 'appetizer', 'Our famous chilli, hand cut, cheese fries. Topped with sour cream, green onion, and real bacon bits', 14);
+
+INSERT INTO food (image, title, category, description, price) VALUES ('https://keviniscooking.com/wp-content/uploads/2020/05/Fried-Pickle-Spears-square.jpg','Deep Fried Pickles', 'appertizer', 'Delicious deep fried crispy pickles. Your hang over ends now.', 12);
+
+

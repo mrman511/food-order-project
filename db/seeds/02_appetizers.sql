ef56c5ba087d9a36ec7faf693fa31c2fb753e9e5
Paul Bodner <bodner80@gmail.com>
2022-02-13 08:28:08 -0800

    added seeds

---
+++ db/seeds/02_appetizers.sql
@@ -0,0 +1,6 @@
+INSERT INTO food (image, title, category, description, price) VALUES ('image', 'Deep fried spiral-cut whole potato on a skewer', 6.99)
+INSERT INTO food (image, title, category, description, price) VALUES ('image', 'Potato Salad', 'appetizers', 'Boiled potatoes with a variety of other salad ingredients', 5.99)
+
+INSERT INTO food (image, title, category, description, price)
+VALUES ('https://celebrationgeneration.com/wp-content/uploads/2021/05/Cactus-Cut-Potatoes-4.jpg', 'Cactus Cut Fries', 'appetizer', 'Crispy thick cut potato chips. Served with your choice of dipping sauce', 6.99),
+('https://www.corriecooks.com/wp-content/uploads/2018/08/Instant-Pot-Yam-Fries.jpg', 'Yam Fries', 'appetizer', 'Yam friess served with your choice of dipping sauce', 5.99);
